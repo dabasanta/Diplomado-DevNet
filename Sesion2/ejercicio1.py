77c151c26095276e56172e5424ea5ef83e65ad8c
Danilo Basanta <dabasantaco@gmail.com>
2023-06-21 20:17:41 -0500

    Creando archivos clase 1

---
+++ Sesion2/ejercicio1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Write a program to sum two numbers and print the result in console output
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(a + b)
